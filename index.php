de1b0ed3f0329faf86a622550f73554b8279d69a
harias25 <andreaflores2501@gmail.com>
2020-10-09 21:17:59 -0600

    practica9

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 
     <?php 
     //contenido a mostrar en la pagina web 
-        echo '<h1>Practica 8</h1>';
-        echo '<h2>Contenedores: conceptos, Docker, docker compose</h2>';
+        echo '<h1>Practica 9</h1>';
+        echo '<h2>Contenedores: construcción compuesta de Docker compose</h2>';
         //conexion a la base de datos
         $conexion=new PDO('mysql:host=db; dbname=BDPractica8','root', 'hello1234');
         //Query a la tabla alumnos
-        foreach($conexion->query('SELECT * from alumnos') as $fila) {
+        foreach($conexion->query('SELECT * from alumno') as $fila) {
             for ($i = 0; $i <= 2; $i++) {
-              
-                print_r($fila[$i] );
+                echo("\n");
+                echo($fila[$i] );
+                echo("\n");
+                
             }
             
         }
